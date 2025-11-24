a7093051b2d51fccd9bee64a99451147c64f2b67
prakhar24bsa10143 <prakhar.24bsa10143@vitbhopal.ac.in>
2025-11-24 23:02:39 +0530

    Delete Member.java

--- Member.java
+++
@@ -1,6 +0,0 @@
-public class Member {
-    int id; String name, password;
-    public Member(int id, String name, String password) {
-        this.id = id; this.name = name; this.password = password;
-    }
-}

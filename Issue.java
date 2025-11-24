68765fa62e747142cac0936d0f8850af1a33ba41
prakhar24bsa10143 <prakhar.24bsa10143@vitbhopal.ac.in>
2025-11-24 23:01:25 +0530

    Delete Issue.java

--- Issue.java
+++
@@ -1,9 +0,0 @@
-public class Issue {
-    int memberId; String isbn; String issueDate;
-    public Issue(int memberId, String isbn, String issueDate) {
-        this.memberId = memberId; this.isbn = isbn; this.issueDate = issueDate;
-    }
-    public String toString() {
-        return "Member " + memberId + " → Book: " + isbn + " (Issued: " + issueDate + ")";
-    }
-}
